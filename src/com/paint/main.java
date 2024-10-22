b077f1edfff8a14cf30e31e895ceb9009dd21432
davletmegahorosh <azimovdavlet460@gmail.com>
2024-10-22 10:52:18 +0600

    centered button created func

--- src/com/paint/main.java
+++ src/com/paint/main.java
@@ -1,6 +1,7 @@
 package com.paint;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -10,8 +11,24 @@ public class main {
     private JPanel panel1;
     private JFrame jFrame;
 
-    public main(JFrame jFrame) {
-        this.jFrame = jFrame;
+    public main() {
+        panel1 = new JPanel(new GridBagLayout());
+        button1 = new JButton("Выбрать файл");
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.NONE;
+
+        panel1.add(button1, gbc);
+
+        this.jFrame = getjFrame();
+        this.jFrame.setContentPane(panel1);
+        this.jFrame.setVisible(true);
+
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -59,12 +76,18 @@ public class main {
                 fileNameLower.endsWith(".svg");
     }
 
-    public static void main(String[] args) {
+    public JFrame getjFrame() {
         JFrame jFrame = new JFrame();
-        jFrame.setContentPane(new main(jFrame).panel1);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setBounds(450, 350, 500, 300);
         jFrame.setTitle("Megapushka");
         jFrame.setVisible(true);
+        return jFrame;
     }
+
+    public static void main(String[] args) {
+        new main();
+    }
+
+
 }
