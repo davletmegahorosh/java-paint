9807d60a321a7d7dd41ce245261012936fb44bb1
Feruz Tursunov <feruztursunovv@gmail.com>
2024-11-01 22:46:37 +0600

    Spliting on main file to defferent clssses

---
+++ src/com/paint/FileUtils.java
@@ -0,0 +1,9 @@
+package com.paint;
+public class FileUtils {
+    public static boolean isImageFile(String fileName) {
+        String fileNameLower = fileName.toLowerCase();
+        return fileNameLower.endsWith(".jpg") || fileNameLower.endsWith(".jpeg") ||
+                fileNameLower.endsWith(".png") || fileNameLower.endsWith(".gif") ||
+                fileNameLower.endsWith(".svg");
+    }
+}
---
+++ src/com/paint/MainApp.java
@@ -0,0 +1,7 @@
+package com.paint;
+import javax.swing.*;
+public class MainApp {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new FileSelectionPanel().showFrame());
+    }
+}
