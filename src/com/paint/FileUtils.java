89794b65fa744454d6be73e6dbe0e69d6249c011
Feruz Tursunov <feruztursunovv@gmail.com>
2024-11-05 20:54:44 +0600

    Recreated 4 different files: PaintApp, DrawningPanel, Tool, ToolPanel. There features of creating new file, opening existing file, saving file and draw something

--- src/com/paint/FileUtils.java
+++
@@ -1,9 +0,0 @@
-package com.paint;
-public class FileUtils {
-    public static boolean isImageFile(String fileName) {
-        String fileNameLower = fileName.toLowerCase();
-        return fileNameLower.endsWith(".jpg") || fileNameLower.endsWith(".jpeg") ||
-                fileNameLower.endsWith(".png") || fileNameLower.endsWith(".gif") ||
-                fileNameLower.endsWith(".svg");
-    }
-}
--- src/com/paint/MainApp.java
+++
@@ -1,7 +0,0 @@
-package com.paint;
-import javax.swing.*;
-public class MainApp {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new FileSelectionPanel().showFrame());
-    }
-}
