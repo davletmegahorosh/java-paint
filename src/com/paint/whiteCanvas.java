f397ea6f561d4cd884cbece35d9171a9b1350ed5
taesay17 <151002316+taesay17@users.noreply.github.com>
2024-11-05 18:17:42 +0600

    create classes

--- src/com/paint/whiteCanvas.java
+++ src/com/paint/whiteCanvas.java
@@ -4,9 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 
 
-public class WhiteCanvas extends JFrame {
+public class whiteCanvas extends JFrame {
 
-    public WhiteCanvas() {
+    public whiteCanvas() {
         setTitle("New file");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600);
@@ -16,24 +16,20 @@ public class WhiteCanvas extends JFrame {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                // Рисуем белый фон
                 g.setColor(Color.WHITE);
                 g.fillRect(0, 0, getWidth(), getHeight());
                 g.setColor(Color.BLACK);
-                g.drawString("Простой белый лист", 20, 30);
+                g.drawString("White canva", 20, 30);
             }
         };
 
-        // Устанавливаем фон панели на белый
         panel.setBackground(Color.WHITE);
         add(panel);
 
-        // Показываем окно
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        // Запуск приложения в потоке диспетчера событий
-        SwingUtilities.invokeLater(() -> new WhiteCanvas());
+        SwingUtilities.invokeLater(() -> new whiteCanvas());
     }
 }
